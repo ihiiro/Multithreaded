091f7fdae882680acb8c41d84b01cf00a6af14c0
hiro <yassirelyakine@gmail.com>
2022-09-07 14:07:50 +0100

    development on home page initiated(mobile only so far)

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,16 @@
 from app import app
 from flask import render_template
 from datetime import datetime
+from flask import request
 
 @app.route('/')
 def test():
-    current_year = datetime.now().strftime("%Y")
-    return render_template('test_article.html', current_year=current_year)
+    return render_template('test_article.html',
+                           current_year=datetime.now().strftime("%Y"),
+                           request_url=request.url)
+
+@app.route('/home')
+def home():
+    return render_template('home.html',
+                           current_year=datetime.now().strftime("%Y"),
+                           request_url=request.url)
