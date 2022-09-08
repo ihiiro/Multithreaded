5957327f5607a63a1e180311bb72504fbb708df4
hiro <yassirelyakine@gmail.com>
2022-09-09 00:34:08 +0100

    Done with mobile version

--- app/routes.py
+++ app/routes.py
@@ -3,14 +3,14 @@ from flask import render_template
 from datetime import datetime
 from flask import request
 
-@app.route('/')
-def test():
-    return render_template('test_article.html',
-                           current_year=datetime.now().strftime("%Y"),
-                           request_url=request.url)
+@app.route('/home-list<int:num>')
+def home(num):
+    return render_template(f'home{num}.html',
+    current_year=datetime.now().strftime("%Y"),
+    request_url=request.url)
 
-@app.route('/home')
-def home():
-    return render_template('home.html',
+@app.route('/article<int:num>')
+def article(num):
+    return render_template(f'article{num}.html',
                            current_year=datetime.now().strftime("%Y"),
                            request_url=request.url)
